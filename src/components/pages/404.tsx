8593f79252fae993275acf723d06bb59dfba6aac
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-09-25 09:10:03 +0200

    feat:Added a go home button to 404 page

--- src/components/pages/404.tsx
+++ src/components/pages/404.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export default function Error() {
   return (
-<div className='Pic404'>
-    <h1>Oops! Page Not Found</h1>
-    <img src="/src/assets/The-404-Page.jpg" alt="" />
-</div>
+    <div className="Pic404">
+      <h1>Oops! Page Not Found</h1>
+      <img src="/src/assets/The-404-Page.jpg" alt="" />
+
+      <div className="home-btn">
+        <button className="go-home" type="submit">
+          <Link className="link-a" to="/
+          ">
+            Go Home
+          </Link>
+        </button>
+      </div>
+    </div>
   );
 }
